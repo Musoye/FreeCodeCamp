41a4e51e4ec1056a6036ee520aab8c7f5ace9bf2
Musoye <oyebamijimustapha44@gmail.com>
2022-11-03 04:28:19 -1100

    primenumber

---
+++ JS-Data-structure/primenumber.js
@@ -0,0 +1,21 @@
+// this sum find the prime number by using primilaity test.from 2 to square root of the number.
+function sumPrimes(num) {
+    // Helper function to check primality
+    function isPrime(num) {
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0)
+          return false;
+      }
+      return true;
+    }
+  
+    // Check all numbers for primality
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+      if (isPrime(i))
+        sum += i;
+    }
+    return sum;
+  }
+  
+  sumPrimes(10);
