12e05f523c1130fd365dca42b456cdd74262824e
Musoye <oyebamijimustapha44@gmail.com>
2023-01-09 04:32:02 -1100

    update

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-def copy_list(l):
-    return (l.copy())
+"""MyInt inverted eq and does not equal"""
+
+
+class MyInt(int):
+    """hange == and !="""
+
+    def __ne__(self, value):
+        """revert equal"""
+        return (self.real == value)
+
+    def __eq__(self, value):
+        """revert not equal"""
+        return (self.real != value)
